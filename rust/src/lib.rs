ce92b7b487ffb5874b360039fc45de0b627a6f82
Dirk Gadsden <dirk@esherido.com>
2016-01-15 09:57:34 -0500

    Add matcher for Safari user agents

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -41,6 +41,7 @@ enum BrowserFamily {
     Edge,
     Firefox,
     Opera,
+    Safari,
     Other,
 }
 
@@ -51,26 +52,31 @@ impl Browser {
 
         let is_opera        = Browser::is_opera(ua);
         let is_edge         = Browser::is_edge(ua);
-        let is_chrome       = ua.contains("Chrome") && !is_opera && !is_edge;
+        let matched_chrome  = if !is_opera && !is_edge { Browser::match_chrome(ua) } else { None };
         let matched_firefox = Browser::match_firefox(ua);
+        let matched_safari  = if ua.contains("Safari") { Browser::match_safari(ua) } else { None };
 
         let family =
-            if is_chrome {
-                let (major, minor) = Browser::match_chrome(ua);
-                major_version = major;
-                minor_version = minor;
-                BrowserFamily::Chrome
-
-            } else if is_edge {
+            if is_edge {
                 BrowserFamily::Edge
 
             } else if is_opera {
                 BrowserFamily::Opera
 
+            } else if let Some((major, minor)) = matched_chrome {
+                major_version = major;
+                minor_version = minor;
+                BrowserFamily::Chrome
+
             } else if let Some((major, minor)) = matched_firefox {
                 major_version = major;
                 minor_version = minor;
                 BrowserFamily::Firefox
+
+            } else if let Some((major, minor)) = matched_safari {
+                major_version = major;
+                minor_version = minor;
+                BrowserFamily::Safari
 
             } else {
                 BrowserFamily::Other
@@ -95,22 +101,31 @@ impl Browser {
     pub fn match_firefox(ua: &str) -> Option<(i8, i8)> {
         let re = Regex::new(r"Firefox/(\d+)\.(\d+)").unwrap();
 
-        match re.captures(ua) {
-            Some(captures) => {
-                let major_version = i8::from_str(&captures[1]).unwrap();
-                let minor_version = i8::from_str(&captures[2]).unwrap();
-                Some((major_version, minor_version))
-            },
-            None => None,
-        }
+        re.captures(ua).map(|captures| {
+            let major_version = i8::from_str(&captures[1]).unwrap();
+            let minor_version = i8::from_str(&captures[2]).unwrap();
+            (major_version, minor_version)
+        })
     }
 
-    pub fn match_chrome(ua: &str) -> (i8, i8) {
-        let re            = Regex::new(r"(Chromium|Chrome)/(\d+)\.(\d+)").unwrap();
-        let captures      = re.captures(ua).unwrap();
-        let major_version = i8::from_str(&captures[2]).unwrap();
-        let minor_version = i8::from_str(&captures[3]).unwrap();
-        (major_version, minor_version)
+    pub fn match_chrome(ua: &str) -> Option<(i8, i8)> {
+        let re = Regex::new(r"(Chromium|Chrome)/(\d+)\.(\d+)").unwrap();
+
+        re.captures(ua).map(|captures| {
+            let major_version = i8::from_str(&captures[2]).unwrap();
+            let minor_version = i8::from_str(&captures[3]).unwrap();
+            (major_version, minor_version)
+        })
+    }
+
+    pub fn match_safari(ua: &str) -> Option<(i8, i8)> {
+        let re = Regex::new(r"Version/(\d+)\.(\d+)(?:\.\d+)? Safari").unwrap();
+
+        re.captures(ua).map(|captures| {
+            let major_version = i8::from_str(&captures[1]).unwrap();
+            let minor_version = i8::from_str(&captures[2]).unwrap();
+            (major_version, minor_version)
+        })
     }
 }
 
@@ -167,6 +182,7 @@ pub extern "C" fn get_browser_family(ua: *const UserAgent) -> *mut c_char {
         BrowserFamily::Edge    => "Edge",
         BrowserFamily::Firefox => "Firefox",
         BrowserFamily::Opera   => "Opera",
+        BrowserFamily::Safari  => "Safari",
         BrowserFamily::Other   => "Other",
     };
 
@@ -185,7 +201,21 @@ mod tests {
     #[test]
     fn test_match_firefox() {
         let did_match = Browser::match_firefox("Firefox/1.2");
+        assert_eq!(did_match, Some((1, 2)));
 
-        assert_eq!(did_match, Some((1, 2)))
+        let didnt_match = Browser::match_firefox("NotFirefox/x.y");
+        assert_eq!(didnt_match, None)
+    }
+
+    #[test]
+    fn test_match_safari() {
+        let version7 = Browser::match_safari("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A");
+        assert_eq!(version7, Some((7, 0)));
+
+        let version5 = Browser::match_safari("Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-us) AppleWebKit/534.1+ (KHTML, like Gecko) Version/5.0 Safari/533.16");
+        assert_eq!(version5, Some((5, 0)));
+
+        let mobile_version6 = Browser::match_safari("Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25");
+        assert_eq!(mobile_version6, None)
     }
 }
