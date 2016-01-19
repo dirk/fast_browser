57974a5a82309ea3ba2492c2ac508d8b3e5e4b17
Dirk Gadsden <dirk@esherido.com>
2016-01-19 12:19:58 -0500

    Improve benchmark testing

--- test/benchmark/compare.rb
+++ test/benchmark/compare.rb
@@ -18,21 +18,30 @@ class TestCompare < Minitest::Test
     # Make sure the Rust lib is hot
     _ = FastBrowser.new ua
 
-    Benchmark.bm(12) do |x|
-      x.report('browser:') {
-        TIMES.times do
-          b = Browser.new user_agent: ua
-          assert_equal b.firefox?, true
-          assert_equal "40", b.version
-        end
-      }
-      x.report('fast_browser:') {
-        TIMES.times do
-          b = FastBrowser.new ua
-          assert_equal b.firefox?, true
-          assert_equal 40, b.major_version
-        end
-      }
+    test_browser = lambda do
+      TIMES.times do
+        b = Browser.new user_agent: ua
+        assert_equal b.firefox?, true
+        assert_equal "40", b.version
+      end
+    end
+
+    test_fast_browser = lambda do
+      TIMES.times do
+        b = FastBrowser.new ua
+        assert_equal b.firefox?, true
+        assert_equal 40, b.major_version
+      end
+    end
+
+    {
+      'browser' => test_browser,
+      'fast_browser' => test_fast_browser
+    }.each do |(name, test_block)|
+      time    = benchmark_time &test_block
+      objects = benchmark_memory &test_block
+
+      print "#{name.ljust(15)}%10.5f seconds\t%d objects\n" % [time, objects]
     end
   end
 
@@ -41,5 +50,23 @@ class TestCompare < Minitest::Test
   def profile(&block)
     result = RubyProf.profile &block
     RubyProf::FlatPrinter.new(result).print(STDOUT)
+  end
+
+  def benchmark_time &block
+    Benchmark.realtime &block
+  end
+
+  def benchmark_memory &block
+    GC.start
+
+    before_allocated = GC.stat[:total_allocated_objects]
+    GC.disable
+
+    block.call
+
+    after_allocated = GC.stat[:total_allocated_objects]
+    GC.enable
+
+    return after_allocated - before_allocated
   end
 end
