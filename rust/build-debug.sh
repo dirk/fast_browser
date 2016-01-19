260d061bf0c6980b3f671077b9cc9c24bf2d2fcd
Dirk Gadsden <dirk@esherido.com>
2016-01-19 12:25:43 -0500

    Improve Rust development build script

--- rust/build-debug.sh
+++ rust/build-debug.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+
+# Call this like `TARGET=release ./build-debug.sh` to do a release build
+
+# TODO: Make this set the right extension based on the current platform
 NATIVE_EXT=dylib
-cargo build && cp target/debug/libfast_browser.$NATIVE_EXT ../ext/fast_browser/libfast_browser.$NATIVE_EXT
+
+if [ "$TARGET" == "release" ]; then
+  CARGO_ARGS=--release
+  TARGET=release
+else
+  CARGO_ARGS=
+  TARGET=debug
+fi
+
+set -x
+cargo build $CARGO_ARGS && cp target/$TARGET/libfast_browser.$NATIVE_EXT ../ext/fast_browser/libfast_browser.$NATIVE_EXT
