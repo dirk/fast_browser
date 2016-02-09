f8673c430340b7af163bf05a39bb879a316a4a58
Dirk Gadsden <dirk@esherido.com>
2016-02-08 17:40:25 -0800

    Add Mac platform matcher with version capturing

--- rust/src/platform.rs
+++ rust/src/platform.rs
@@ -83,10 +83,12 @@ lazy_static! {
     static ref MATCH_SEQUENCE: Vec<MatchTuple> = {
         use self::PlatformName::*;
 
-        let ios_pattern = r"CPU (?:iPhone )?OS ([0-9])_([0-9])(?:_[0-9])? like Mac OS X";
+        let ios_pattern = r"CPU (?:iPhone )?OS (\d+)_(\d+)(?:_\d+)? like Mac OS X";
+        let mac_pattern = r"Mac OS X (\d+)_(\d+)(?:_\d+)?";
 
         vec![
             (MatchPattern::with_regex(ios_pattern).unwrap(), IOS,     -1, -1),
+            (MatchPattern::with_regex(mac_pattern).unwrap(), Mac,     -1, -1),
             (MatchPattern::with_str("Android"),              Android, 0, 0),
             (MatchPattern::with_str("Linux"),                Linux,   0, 0),
             (MatchPattern::with_str("Macintosh"),            Mac,     0, 0),
@@ -109,6 +111,7 @@ mod tests {
 
     const ANDROID_444: &'static str = "Mozilla/5.0 (Linux; Android 4.4.4; One Build/KTU84L.H4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.135 Mobile Safari/537.36";
     const IOS_802: &'static str     = "Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4";
+    const MAC_1093: &'static str    = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A";
     const WINDOWS_81: &'static str  = "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko";
 
     #[test]
@@ -124,6 +127,14 @@ mod tests {
         assert_eq!(
             Platform::parse(IOS_802),
             Some(Platform::new(IOS, 8, 0))
+        )
+    }
+
+    #[test]
+    fn matches_mac1093() {
+        assert_eq!(
+            Platform::parse(MAC_1093),
+            Some(Platform::new(Mac, 10, 9))
         )
     }
 
